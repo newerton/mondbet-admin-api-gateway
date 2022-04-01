20081b9ecceeb07704ceda261ef68a5388473350
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-03-31 23:40:03 -0300

    chore: create manager roles migration

---
+++ src/database/typeorm/migrations/1648587853216-CreateManagerRoles.ts
@@ -0,0 +1,109 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+  TableIndex,
+} from 'typeorm';
+
+export class CreateManagerRoles1648587853216 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'manager_role',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'manager_id',
+            type: 'uuid',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'resource',
+            type: 'varchar',
+          },
+          {
+            name: 'manager',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'create',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'read',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'update',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'delete',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'visible',
+            type: 'boolean',
+            default: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            default: null,
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'manager_role',
+      new TableForeignKey({
+        name: 'fk-manager_role-manager_id',
+        columnNames: ['manager_id'],
+        referencedTableName: 'manager',
+        referencedColumnNames: ['id'],
+        onUpdate: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createIndex(
+      'manager_role',
+      new TableIndex({
+        name: 'idx_manager_id',
+        columnNames: ['manager_id'],
+        isUnique: true,
+        parser: 'btree',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('manager_role');
+  }
+}
