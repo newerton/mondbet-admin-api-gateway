d67aae095f6606b7e5dbfc60ce7e73dece1a5960
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-04-02 13:32:14 -0300

    chore: add roles validation and fix the currency validation

--- src/common/utils/currency.ts
+++ src/common/utils/currency.ts
@@ -1,15 +1,23 @@
 import currency from 'currency.js';
 
-export const stringToFloat = (value: string): number => {
-  if (isNaN(parseFloat(value))) {
-    const currencyFormatted = currency(value.replace(' ', ''), {
-      symbol: 'R$',
-      separator: '.',
-      decimal: ',',
-      precision: 2,
-    });
+const real = (value: any) =>
+  currency(value, { symbol: 'R$', separator: '.', decimal: ',', precision: 2 });
 
-    return currencyFormatted.value;
+export const stringToFloat = (value: string): number => {
+  const number = currency(value.replace(/[^\d]/g, ''), {
+    fromCents: true,
+    precision: 2,
+  });
+  if (!isNaN(number.value)) {
+    return number.value;
   }
-  return parseFloat(value);
+
+  return null;
+};
+
+export const currencyToDecimal = (string: any): any => {
+  if (string) {
+    return real(string);
+  }
+  return null;
 };
